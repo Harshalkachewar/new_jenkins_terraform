37eff712e2a2107284568d0653b94b9b694000f6
Harshalkachewar <111994225+Harshalkachewar@users.noreply.github.com>
2025-12-02 10:24:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,26 @@
-resource "null_resource" "create_dir_file" {
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      host     = "192.168.1.190"
-      user     = "root"
-      password = "redhat"
+terraform {
+  required_providers {
+    linux = {
+      source  = "mavidser/linux"
+      version = "1.0.2"
     }
-
-    inline = [
-      "mkdir -p /tmp/mydir",
-      "echo 'Hello from Terraform!' > /tmp/mydir/hello.txt"
-    ]
   }
+}
+
+provider "linux" {
+  host     = "192.168.1.190"
+  port     = 22
+  user     = "root"
+  password = "redhat"
+}
+
+# Create a file on the remote Linux server
+resource "linux_file" "myfile" {
+  path    = "/mnt/myfile.txt"
+  content = "Hello from Terraform via Jenkins!"
+}
+
+# Example: Create a directory
+resource "linux_directory" "mydir" {
+  path = "/mnt/mydir"
 }
