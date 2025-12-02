9579a3bfe8b15a3ce07fc3868b9c83b640cef25b
Harshalkachewar <111994225+Harshalkachewar@users.noreply.github.com>
2025-12-02 10:12:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "null_resource" "create_dir_file" {
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
-      host        = "your_server_ip"
-      user        = "your_ssh_user"
-      private_key = file("~/.ssh/id_rsa")
+      host        = "192.168.1.190"
+      user        = "root"
+      password = "redhat"
     }
 
     inline = [
